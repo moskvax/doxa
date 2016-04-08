60f356e138e6c704932bea263046655f39a5af2a
Stephen Caraher <moskvax@gmail.com>
2016-04-08 14:36:39 +1000

    shelve is working to persist the tags now.
    the problem was with region sets being defined in the shelve as
    references to v.sel(), which is constantly changing and becoming empty

--- doxa.py
+++ doxa.py
@@ -1,42 +1,51 @@
 import sublime, sublime_plugin, shelve
-# thread module has been renamed to _thread in python 3
-try:
-    import _thread
-except ImportError:
-    import thread as _thread
 
-class TagManager(object):
-	@classmethod
-	def load(cls):
-		cls.tagshelves = {}
+tagshelves = {}
 
-TagManager.load()
-
-class AddTagRegion(sublime_plugin.TextCommand):
-	def run(self, edit, tag):
-		v = self.view
-		tagshelve = TagManager.tagshelves[v.file_name()]
-
-		old_regions = v.get_regions(tag)
-		print("old_regions == %s" % old_regions)
-		new_regions = v.sel()
-		new_regions.add_all(old_regions)
-		print("new_regions == %s" % new_regions)
-		tagshelve[tag] = new_regions
-		tagshelve.sync()
-		# scope (third arg) == tag for prototyping purposes
-		v.add_regions(tag, new_regions, tag, '')
-
-class InitTagFile(sublime_plugin.EventListener):
+class TagFileManager(sublime_plugin.EventListener):
 	def on_load(self, view):
+		global tagshelves
 		filename = view.file_name()
 		if "TAGTEST_" in filename:
 			tagshelve = shelve.open(filename + '.tags')
-			TagManager.tagshelves[filename] = tagshelve
+			tagshelves[filename] = tagshelve
+			print("*** on_load ***")
 			for tag, saved_regions in tagshelve.items():
-				view.add_regions(tag, saved_regions, tag, '')
+				print("tagshelve['%s'] == %s" % (tag, [x for x in saved_regions]))
+				view.add_regions(tag, saved_regions, tag, '', sublime.PERSISTENT)
 
 	def on_close(self, view):
+		global tagshelves
 		filename = view.file_name()
 		if "TAGTEST_" in filename:
-			TagManager.tagshelves[filename].close()
+			tagshelve = tagshelves[filename]
+			print("*** on_close ***")
+			for tag, saved_regions in tagshelve.items():
+				print("tagshelve['%s'] == %s" % (tag, [x for x in saved_regions]))
+			tagshelves[filename].close()
+
+class AddTagRegion(sublime_plugin.TextCommand):
+	def run(self, edit, tag):
+		print("*** run ***")
+		global tagshelves
+		v = self.view
+		tagshelve = tagshelves[v.file_name()]
+
+		# get currently tagged regions as a list
+		regions = v.get_regions(tag)
+		print("(old) regions == %s" % regions)
+
+		# make a list out of the current view selections and add to the list of regions
+		regions += [r for r in v.sel()]
+		print("(new) regions == %s" % regions)
+
+		# update tagshelve
+		tagshelve[tag] = regions
+		tagshelve.sync()
+
+		# scope (third arg) == tag for prototyping purposes
+		# update ui
+		v.add_regions(tag, regions, tag, '', sublime.PERSISTENT)
+		for tag, saved_regions in tagshelve.items():
+			print("tagshelve['%s'] == %s" % (tag, [x for x in saved_regions]))
+
