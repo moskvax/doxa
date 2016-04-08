295cd1062160872843796f2e6e526f3f8bff8a5c
Stephen Caraher <moskvax@gmail.com>
2016-04-08 13:29:15 +1000

    use shelve library module to attempt tag persistence. currently tags are not being reloaded for some reason.

--- doxa.py
+++ doxa.py
@@ -1,35 +1,42 @@
-import sublime, sublime_plugin, os, os.path
+import sublime, sublime_plugin, shelve
 # thread module has been renamed to _thread in python 3
 try:
     import _thread
 except ImportError:
     import thread as _thread
 
-class TagThreadManager(object):
+class TagManager(object):
 	@classmethod
 	def load(cls):
-		cls.metadata_files = {}
+		cls.tagshelves = {}
 
-    @classmethod
-    def is_enabled(cls, view):
-        try:
-            filename = view.file_name()
-            if filename[-5:] == "_TAG_":
-                if not filename in cls.snapfiles.keys():
-                    cls.snapfiles[filename] = open(filename + ".tags", "a")
-                return True
-            else:
-                return False
-        except:
-            return False
+TagManager.load()
 
 class AddTagRegion(sublime_plugin.TextCommand):
-	def run(self, edit, scope):
-
+	def run(self, edit, tag):
 		v = self.view
+		tagshelve = TagManager.tagshelves[v.file_name()]
 
-		# tag = scope for prototyping purposes
-		tag = scope
+		old_regions = v.get_regions(tag)
+		print("old_regions == %s" % old_regions)
+		new_regions = v.sel()
+		new_regions.add_all(old_regions)
+		print("new_regions == %s" % new_regions)
+		tagshelve[tag] = new_regions
+		tagshelve.sync()
+		# scope (third arg) == tag for prototyping purposes
+		v.add_regions(tag, new_regions, tag, '')
 
-		v.add_regions(tag, v.sel(), scope, '', (sublime.PERSISTENT|sublime.DRAW_NO_OUTLINE))
+class InitTagFile(sublime_plugin.EventListener):
+	def on_load(self, view):
+		filename = view.file_name()
+		if "TAGTEST_" in filename:
+			tagshelve = shelve.open(filename + '.tags')
+			TagManager.tagshelves[filename] = tagshelve
+			for tag, saved_regions in tagshelve.items():
+				view.add_regions(tag, saved_regions, tag, '')
 
+	def on_close(self, view):
+		filename = view.file_name()
+		if "TAGTEST_" in filename:
+			TagManager.tagshelves[filename].close()
